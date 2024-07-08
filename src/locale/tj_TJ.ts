6376fe291099ca14f1a019bcf8e11c898b567091
Abubakr7 <abubakr.juraev98@gmail.com>
2024-07-08 17:08:09 +0500

    add tajik language

---
+++ src/locale/tj_TJ.ts
@@ -0,0 +1,21 @@
+
+import type { PaginationLocale } from '../interface';
+
+const locale: PaginationLocale = {
+    // Options
+    items_per_page: '/ саҳифа',
+    jump_to: 'Ба',
+    jump_to_confirm: 'тасдиқ',
+    page: 'Саҳифа',
+
+    // Pagination
+    prev_page: 'Саҳифаи қаблӣ',
+    next_page: 'Саҳифаи оянда',
+    prev_5: '5 Саҳифаҳои қаблӣ',
+    next_5: '5 Саҳифаҳои оянда',
+    prev_3: '3 Саҳифаҳои қаблӣ',
+    next_3: '3 Саҳифаҳои оянда',
+    page_size: 'Андозаи саҳифа',
+};
+
+export default locale;
